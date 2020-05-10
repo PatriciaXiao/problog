8f8a1147480c3f2f63a90c67b06deda1657b9f17
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-05-09 18:12:15 -0700

    Update CS267A_HW5_Q1.pl

--- CS267A_HW5_Q1.pl
+++ CS267A_HW5_Q1.pl
@@ -1,20 +1,21 @@
 % reference:
 % https://github.com/ML-KULeuven/problog/blob/master/test/4_bayesian_net.pl
 
+person(john).
+person(mary).
+
 0.7::burglary.
 0.2::earthquake.
-0.9::p_alarm1.
-0.8::p_alarm2.
-0.1::p_alarm3.
 
-alarm :- burglary, earthquake, p_alarm1.
-alarm :- burglary, \+earthquake, p_alarm2.
-alarm :- \+burglary, earthquake, p_alarm3.
+0.9::alarm :- burglary, earthquake.
+0.8::alarm :- burglary, \+earthquake.
+0.1::alarm :- \+burglary, earthquake.
 
+0.8::calls(X) :- alarm, person(X).
+0.1::calls(X) :- \+alarm, person(X).
 
-%%% Evidence
-evidence(alarm,true).
+evidence(calls(john),true).
+evidence(calls(mary),true).
 
-%%% Queries
 query(burglary).
 query(earthquake).
