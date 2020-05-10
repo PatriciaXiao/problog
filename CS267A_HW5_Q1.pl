f8654f975ff06a9150f08f0d8f779e029653ff91
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-05-09 17:57:50 -0700

    Update CS267A_HW5_Q1.pl

--- CS267A_HW5_Q1.pl
+++ CS267A_HW5_Q1.pl
@@ -1,39 +1,20 @@
 % reference:
 % https://github.com/ML-KULeuven/problog/blob/master/test/4_bayesian_net.pl
 
-% helper
-do_list(N, L):- 
-    findall(Num, between(1, N, Num), L).
+0.7::burglary.
+0.2::earthquake.
+0.9::p_alarm1.
+0.8::p_alarm2.
+0.1::p_alarm3.
 
-member_classic(X, [X|Xs]).
-member_classic(X, [Y|Ys]) :-
-    member_classic(X, Ys).
-
-append([], X, X).
-append([X | Y], Z, [X | W]) :- append( Y, Z, W).
-
-0.5::heads(C, N) :- coin(C, N).
-
-coin(C, N) :- do_list(N, List), member_classic(C, List).
-
-% M consecutives in N flips
-consecutive(M,N) :- do_list(N, List), consecutive(List,N,M,0). % all possible flips
+alarm :- burglary, earthquake, p_alarm1.
+alarm :- burglary, \+earthquake, p_alarm2.
+alarm :- \+burglary, earthquake, p_alarm3.
 
 
-% check if the list has a sublist of true that is length M
-consecutive(_,_,M,Len) :- Len >= M.
-consecutive([Hd|Tl],N,M,Len) :- 
-    heads(Hd,N),
-    IncLen is Len + 1,
-    consecutive(Tl, N, M, IncLen).
-consecutive([Hd|Tl],N,M,Len) :- 
-    \+heads(Hd,N),
-    consecutive(Tl, N, M, 0).
+%%% Evidence
+evidence(alarm,true).
 
 %%% Queries
-query(consecutive(2, 5)).
-
-
-
-%someHeads :- heads(_, 2).
-%query(someHeads).
+query(burglary).
+query(earthquake).
